5b28292bc858864f255cc5e1034361c6b7158415
Lwvzi <lwazizozi@gmail.com>
2022-09-27 22:27:13 +0200

    created + styled reviews section

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-$('.owl-carousel').owlCarousel({
+$('#slide').owlCarousel({
 
     loop: true,
     margin: 21,
@@ -17,6 +17,33 @@ $('.owl-carousel').owlCarousel({
         },
         1000:{
             items: 3
+        }
+    }
+
+});
+
+
+//Review Slider
+
+$('.owl-carousel').owlCarousel({
+
+    loop: true,
+    margin: 21,
+    smartSpeed: 1400,
+    nav:false,
+    dots:false,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    autoplayHoverPause: true,
+    responsive:{
+        0:{
+            items: 1
+        },
+        600:{
+            items: 2
+        },
+        1000:{
+            items: 2
         }
     }
 
